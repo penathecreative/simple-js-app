3215e8a4c4bbbc69ee22685e2b49c8160037e5b7
penathecreative <149909951+penathecreative@users.noreply.github.com>
2023-12-06 20:19:26 +0100

    Committing to a new structure
    New pokemonList Variable creation, with various attributes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert("Hello world");
-let favoriteFood = "Cachupa";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Charmander", type: "fire", hight: "0.6" },
+  { name: "Squirtle", type: "Water", hight: "0.5" },
+  { name: "Bulbasaur", type: "Grass", hight: "0.7" },
+];
+console.log(pokemonList);
