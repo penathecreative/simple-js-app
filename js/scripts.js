da3c7a74696e66ea6e914bd6e7d701bca272935e
penathecreative <149909951+penathecreative@users.noreply.github.com>
2023-12-13 17:07:29 +0100

    JS Code Committ
    Code had to be re-wrote  as the former didn't functioned properly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,33 @@
-let pokemonList = [
-  { name: "Charmander", type: ["fire"], height: "0.6" },
-  { name: "Squirtle", type: ["Water"], height: "0.5" },
-  { name: "Bulbasaur", type: ["Grass"], height: "0.7" },
-];
+const description = (pokemon) => {
+  if (pokemon.height > 1.5) {
+    return " - Wow! That's big!";
+  } else if (pokemon.height >= 0.5 && pokemon.height <= 1.5) {
+    return " - Average size Pokemon ";
+  } else {
+    return " - Small Pokemon ";
+  }
+};
 
+//Pokemons with the height>0.5, and <1.5 will be considered average size. Height > 1.5 are consider big pokemn.
+//The rest will be considered as small
+
+let pokemonList = [
+  { name: "Charmander", type: ["fire"], height: "1.6" }, //pokemon objects
+  { name: "Squirtle", type: ["Water"], height: "0.5" },
+  { name: "Bulbasaur", type: ["Grass"], height: "0.2" },
+];
 for (let i = 0; i < pokemonList.length; i++) {
-  //Condition that establishes the heigth from pokemons, using the array pokemonList = i
   document.write(
-    pokemonList[i].name +
+    "<p>" +
+      pokemonList[i].name +
       " " +
       "(height: " +
       pokemonList[i].height +
       ")" +
-      "<br>"
+      description(pokemonList[i]) +
+      "" +
+      "<p>"
   );
-  if (pokemonList[i].height > 1.5) {
-    document.write(" - Wow! That's big!");
-  } else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1.5) {
-    document.write(
-      pokemonList[i].name +
-        "  " +
-        "(height:" +
-        "  " +
-        pokemonList[i].height +
-        " ) " +
-        "Average size Pokemon" +
-        "<br>"
-    );
-  } else {
-    document.write(
-      pokemonList[i].name +
-        "  " +
-        "(height:" +
-        "  " +
-        pokemonList[i].height +
-        " ) " +
-        "Small Pokemon" +
-        "<br>"
-    );
-  }
 }
 
-//Pokemons with the height>0.5, and <1.5 will be considered average size. Height > 1.5 are consider big pokemn.
-//The rest will be considered as small
+// Condition that establishes the height from pokemons, using the array pokemonList = i
